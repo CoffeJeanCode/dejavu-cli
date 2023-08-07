694392e11c9812f93f782c8f23b3d0b4d96b8148
CoffeJeanCode <ortizmurciajeanpierre@gmail.com>
2023-08-07 22:31:09 +0000

    feat: add create file if not exist method

--- src/services/file-manager.service.ts
+++ src/services/file-manager.service.ts
@@ -4,29 +4,28 @@ import { Config } from "../models/config.model";
 export class FileManager {
   private configPath = "./dejavu.json";
 
-  getConfig(): Config | null {
+  getConfig = (): Config | null => {
     try {
       const data = fs.readFileSync(this.configPath, "utf8");
       return JSON.parse(data) as Config;
     } catch (error) {
       return null;
     }
-  }
+  };
 
-  saveConfig(config: Config): void {
+  saveConfig = (config: Config): void => {
     fs.writeFileSync(this.configPath, JSON.stringify(config, null, 2), "utf8");
-  }
+  };
 
-  exists(path: string): Promise<boolean> {
-    return new Promise((resolve) => {
+  exists = (path: string): Promise<boolean> =>
+    new Promise((resolve) => {
       fs.access(path, fs.constants.F_OK, (error) => {
         resolve(!error);
       });
     });
-  }
 
-  readFile(path: string): Promise<string> {
-    return new Promise((resolve, reject) => {
+  readFile = (path: string): Promise<string> =>
+    new Promise((resolve, reject) => {
       fs.readFile(path, "utf8", (error, data) => {
         if (error) {
           reject(error);
@@ -35,10 +34,9 @@ export class FileManager {
         }
       });
     });
-  }
 
-  writeFile(path: string, content: string): Promise<void> {
-    return new Promise((resolve, reject) => {
+  writeFile = (path: string, content: string): Promise<void> =>
+    new Promise((resolve, reject) => {
       fs.writeFile(path, content, "utf8", (error) => {
         if (error) {
           reject(error);
@@ -47,10 +45,9 @@ export class FileManager {
         }
       });
     });
-  }
 
-  createDirectoryIfNotExists(path: string): Promise<void> {
-    return new Promise((resolve, reject) => {
+  createDirectoryIfNotExists = (path: string): Promise<void> =>
+    new Promise((resolve, reject) => {
       this.exists(path).then((exists) => {
         if (!exists) {
           fs.mkdir(path, { recursive: true }, (error) => {
@@ -65,5 +62,21 @@ export class FileManager {
         }
       });
     });
-  }
+
+  createFileIfNotExists = (path: string, content: string): Promise<void> =>
+    new Promise((resolve, reject) => {
+      this.exists(path).then((exists) => {
+        if (!exists) {
+          fs.writeFile(path, content, "utf8", (error) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve();
+            }
+          });
+        } else {
+          resolve();
+        }
+      });
+    });
 }
