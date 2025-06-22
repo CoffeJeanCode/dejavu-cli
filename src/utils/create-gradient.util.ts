d6a8cdb1ea9367496d509f77afbe3d3fc99a010e
Jean Pierre Ortiz <ortizmurciajeanpierre@gmail.com>
2025-06-22 02:37:19 +0000

    feat: add serveral utils

--- src/utils/create-gradient.util.ts
+++ src/utils/create-gradient.util.ts
@@ -12,19 +12,21 @@ export const createGradient = (
     const ratio = i / gradientSteps;
     const r = Math.floor(
       (1 - ratio) * parseInt(startColor.slice(1, 3), 16) +
-        ratio * parseInt(endColor.slice(1, 3), 16)
+      ratio * parseInt(endColor.slice(1, 3), 16)
     );
     const g = Math.floor(
       (1 - ratio) * parseInt(startColor.slice(3, 5), 16) +
-        ratio * parseInt(endColor.slice(3, 5), 16)
+      ratio * parseInt(endColor.slice(3, 5), 16)
     );
     const b = Math.floor(
       (1 - ratio) * parseInt(startColor.slice(5, 7), 16) +
-        ratio * parseInt(endColor.slice(5, 7), 16)
+      ratio * parseInt(endColor.slice(5, 7), 16)
     );
+
     const color = `#${r.toString(16).padStart(2, "0")}${g
       .toString(16)
       .padStart(2, "0")}${b.toString(16).padStart(2, "0")}`;
+
     gradientTitle.push(chalk.bgHex(color)(title[i]));
   }
 
